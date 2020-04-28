ba1262b1114d2acf7d0cdaa1974d57dcdccf847f
Patrick Nyam <ssnyam@yahoo.com>
2020-04-28 16:11:38 +0000

    6th commit

--- devops01b.sh
+++ devops01b.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
-#The script is used to gather disk space and volumes. Sik Nyam 4/2020
+#The script is used to display info. Sik Nyam 4/2020
 #
-if [ -z "$1" ]; then
-	echo "Please enter an argument(Choices: -volumes, -cpu, -ram, -network, -all): "
-exit
-fi
+case $1 in
+                                                                                                 
+-volume)
+	echo "=====Volumes and file system====="
+	/bin/df -h
+  ;;
+-cpu)
+	echo "=====CPU detail====="
+	/bin/cat /proc/cpuinfo
+  ;;
+-ram)
+  echo "=====Memory utilization====="
+	/usr/bin/free -m
+  ;;
+-network)
+  echo "=====Network and MAC Info====="
+	/sbin/ip add | grep ether
+  ;;
+-all)
+	echo "=====Volumes and file system====="
+	/bin/df -h
+	echo " "
+	echo "=====CPU detail====="
+	/bin/cat /proc/cpuinfo
+	echo " "
+  	echo "=====Memory utilization====="
+	/usr/bin/free
+	echo " "
+  	echo "=====Network Info====="
+	/sbin/ip add | grep ether
+	echo " "
+;;
+*)
+  echo "Usage: $0 ARG (Choices: -volumes, -cpu, -ram, -network, -all):"
+  ;;
+esac
 #
-if [ $1 == -volumes ]; then
-/bin/df -h			> /home/patrick/DevOps01/devops01b.$1
-exit
-#
-if [ $1 == -cpu ]; then
-/bin/cat /proc/cpuinfo		> /home/patrick/DevOps01/devops01b.$1
-exit
-#
-elif [ $1 == -ram ]; then
-/usr/bin/free			> /home/patrick/DevOps01/devops01b.$1
-exit
-#
-elif [ $1 == -network ]; then
-/sbin/ip add | grep ether	> /home/patrick/DevOps01/devops01b.$1
-exit
-#
-elif [ $1 == -all ]; then
-/bin/cat /proc/cpuinfo          > /home/patrick/DevOps01/devops01b.$1
-/usr/bin/free                   >> /home/patrick/DevOps01/devops01b.$1
-/sbin/ip add | grep ether	>> /home/patrick/DevOps01/devops01b.$1
-fi
-
